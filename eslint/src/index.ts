b626595358fec35b1a95e66caeaa0dd3ac0bf221
Danil Karpenko <limpix31@gmail.com>
2023-08-27 13:28:25 +0200

    feat: remove eslint plugin

--- eslint/src/index.ts
+++
@@ -1,6 +0,0 @@
-import type { TSESLint } from '@typescript-eslint/utils'
-import { importRule } from './rule.js'
-
-export const rules = {
-	import: importRule,
-} satisfies Record<string, TSESLint.RuleModule<string, Array<unknown>>>
